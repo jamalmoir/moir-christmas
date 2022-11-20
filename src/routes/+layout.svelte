d6320e716fe069cb10979a9067e861e6b0589c39
Jamal Moir <jamal.moir@gmail.com>
2022-11-20 18:16:07 +0900

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+    import "../app.postcss";
+    import Particles from "svelte-particles";
+    // import { loadFull } from "tsparticles";
+    import {loadSnowPreset} from "tsparticles-preset-snow"
+
+    let particlesOptions = {
+        background: {
+            color: {
+                value: "none",
+            },
+        },
+        preset: "snow",
+    };
+
+    let particlesInit = async (engine) => {
+        await loadSnowPreset(engine);
+    };
+</script>
+
+<main class="w-full h-screen bg-slate-800">
+    <slot />
+    <div class="z-0">
+        <Particles
+            id="tsparticles"
+            options={particlesOptions}
+            particlesInit={particlesInit}
+        />
+    </div>
+</main>
+
+<style global>
+    :root {
+        font-family: 'Karla', sans-serif;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="w-full h-screen flex flex-col justify-center items-center">
+    <div class="flex flex-col rounded-lg bg-white p-8 md:p-24 gap-4">
+        <div class="flex flex-col">
+            <div class="text-xs md:text-base">モイア<span class="text-red-900 text-bold">クリスマス</span>２０２２年</div>
+            <h1 class="text-slate-900 text-3xl md:text-6xl text-bold">MOIR <span class="text-red-900 text-bold">CHRISTMAS</span> 2022</h1>
+            <div class="text-xs md:text-base self-end">クリスマスディナー・お酒・プレゼント交換・ゲーム</div>
+        </div>
+
+        <div class="flex flex-col">
+            <div class="flex flex-row gap-1">
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/mai.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/jamal.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/shimi.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/kiuchi.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/tassan.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/tsukki.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/ogata.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/yuriko.png" alt="person"/>
+                <img class="w-8 h-8 hover:animate-bounce" src="/people/hiroya.png" alt="person"/>
+            </div>
+        </div>
+    </div>
+</div>
